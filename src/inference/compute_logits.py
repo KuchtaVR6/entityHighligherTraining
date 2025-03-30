42047a5d714a3f562f2aab74b1dcd441293df2ea
KuchtaVR6 <kuchtavr6@gmail.com>
2025-03-30 22:49:16 +0100

    fix the detokenization

--- src/inference/compute_logits.py
+++ src/inference/compute_logits.py
@@ -10,10 +10,9 @@ def compute_logits(model, infer_dataset, tokenizer):
 
         zero_tensor = torch.zeros_like(input_ids)
 
-        tokens = tokenizer.convert_ids_to_tokens(input_ids.tolist())
-        original_text = batch["text"] if "text" in batch else None
+        tokens = [tokenizer.convert_ids_to_tokens(seq.tolist()) for seq in input_ids]
 
         with torch.no_grad():
             _, logits = model(input_ids=input_ids, attention_mask=attention_mask, labels=zero_tensor)
 
-            print(tokens, original_text, logits.shape)
+            print(tokens, logits.shape)
