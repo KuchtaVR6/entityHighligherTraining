459767125326a61a4ddcae8ab827dd6be02b0b7a
KuchtaVR6 <kuchtavr6@gmail.com>
2025-05-10 21:07:16 +0100

    adjust paths

--- src/configs/path_config.py
+++ src/configs/path_config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 save_model_path = Path("results/custom_model.pth")
-train_data_path = Path("data/toy_train.json")
-eval_data_path = Path("data/toy_eval.json")
+train_data_path = Path("data/split_files/part_1.json")
+eval_data_path = Path("data/toy_train.json")
 logits_data_path = Path("logits/inference_logits.jsonl")
 logits_output_path = Path("logits/output.jsonl")
 checkpoints_path = Path("checkpoints")
