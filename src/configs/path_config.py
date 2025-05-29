0a493941b18349a81a30f0fe1cb4eec82ec1380c
KuchtaVR6 <kuchtavr6@gmail.com>
2025-05-29 19:20:04 +0100

    move the path pointer to part

--- src/configs/path_config.py
+++ src/configs/path_config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 save_model_path = Path("results/custom_model.pth")
-train_data_path = Path("data/split_files/part_1.json")
+train_data_path = Path("data/split_files/part_2.json")
 eval_data_path = Path("data/toy_train.json")
 logits_data_path = Path("logits/inference_logits.jsonl")
 logits_output_path = Path("logits/output.jsonl")
